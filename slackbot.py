4f6ec2fd027de1e6369a7c926029c4f170569441
Chris Dileo <ca.ubc.cs.ugrad.11035>
2016-02-28 11:40:29 -0800

    Added bot_history to monitor channel wide sentiment. Small changes to slackbot base

--- slackbot.py
+++ slackbot.py
@@ -5,36 +5,54 @@ import requests
 
 class Slackbot:
 
-	def __init__(self, token):
-		self.s = requests.Session()
+    # run on initial bot launch to get list of users,create user objects
+    # currently doesn't urn
+    def enumerateUsers(self):
+        # channels = self.sc.server.channels
+        # print channels
+        username = self.sc.server.username
+        print username
+        # raw = self.sc.server
+        # print 'Server is: ' + str(raw)
+        # raw = self.sc.server.channels
+        # print 'Channels: ' + str(raw)
 
-		self.token = token
-		sc = SlackClient(token)
-		if sc.rtm_connect():
-		    while True:
-		        obj = sc.rtm_read()
-		        if len(obj) != 0:
-				    if obj[0].has_key('text'):
-				    	print obj[0]['text']
-				    	self.analyse(obj[0]['text'])
-		        time.sleep(1)
-		else:
-		    print "Connection Failed, invalid token?"
+    def __init__(self, token):
+        self.s = requests.Session()
 
-	# from urllib.parse import urlencode
-	def analyse(self, textToAnalyze):
+        self.token = token
+        sc = SlackClient(token)
+        self.sc = sc
+        historyLength = 20
+        self.history = bot_history(historyLength)
+        
+        if sc.rtm_connect():
+            # self.users = self.enumerateUsers()
+            while True:
+                obj = sc.rtm_read()
+                if len(obj) != 0:
+                    if obj[0].has_key('text'):
+                        print obj[0]['text']
+                        self.analyse(obj[0]['text'])
+                time.sleep(1)
+        else:
+            print "Connection Failed, invalid token?"
 
-		apikey = "d39ed152b9ca4e2b4eb9828c0379a44f33ef0f65"
-		params = {}
+    # from urllib.parse import urlencode
+    def analyse(self, textToAnalyze):
 
-		params['apikey'] = apikey
-		params['outputMode'] = 'json'
+        apikey = "d39ed152b9ca4e2b4eb9828c0379a44f33ef0f65"
+        params = {}
 
-		post_data = {}
-		post_data['text'] = textToAnalyze
-		            
-		post_url = "http://access.alchemyapi.com/calls/text/TextGetEmotion?outputMode=json&apikey=d39ed152b9ca4e2b4eb9828c0379a44f33ef0f65"
+        params['apikey'] = apikey
+        params['outputMode'] = 'json'
 
-		results = self.s.post(url=post_url, data=post_data)
+        post_data = {}
+        post_data['text'] = textToAnalyze
 
-		print("Result" + results.text)
+        post_url = "http://access.alchemyapi.com/calls/text/TextGetEmotion?outputMode=json&apikey=d39ed152b9ca4e2b4eb9828c0379a44f33ef0f65"
+
+        results = self.s.post(url=post_url, data=post_data)
+
+        print("Result" + results.text)
+
