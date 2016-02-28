9d64e720780cfd64809bee16859e48a5ff9e309b
tk baylis <tammy.baylis@gmail.com>
2016-02-27 19:39:13 -0800

    Add 3 fns for sentiment, emotions, top emotion. Updated test script

--- test_textemotionanalysis.py
+++ test_textemotionanalysis.py
@@ -7,4 +7,11 @@ Run `python ./test_textemotionanalysis.py` and results should print out
 from textemotionanalysis import TextEmotionAnalyzer
 
 tea = TextEmotionAnalyzer()
-print tea.get_results()
+print "get_sentiment:"
+print tea.get_sentiment(text="poopies!")
+print "get_emotions:"
+print tea.get_emotions(text="poopies!")
+
+print "get_top_emotion:"
+print tea.get_top_emotion()
+
--- textemotionanalysis.py
+++ textemotionanalysis.py
@@ -6,44 +6,67 @@ Created on Sat Feb 27 16:22:31 2016
 
 @author: Shaylene
 """
+
 import requests
+import json
+import operator
+
 
 class TextEmotionAnalyzer:
 
-    #s = requests.Session()
-
-    def __init__(self):
+    def __init__(self, outputMode="json", apikey="d39ed152b9ca4e2b4eb9828c0379a44f33ef0f65"):
         """
+        Create instance of TextEmotionAnalyzer to use
 
+        Optional params: outputMode, apikey
         """
-
         self.s = requests.Session()
-
-        self.apikey = "d39ed152b9ca4e2b4eb9828c0379a44f33ef0f65"
         self.params = {}
-        self.params['apikey'] = self.apikey
-        self.params['outputMode'] = 'json'
-        self.post_data = {}
-        self.post_data['text'] = "poopies"
+        self.params['apikey'] = apikey
+        self.params['outputMode'] = outputMode
 
-        #post_url = "http://gateway-a.watsonplatform.net/calls/html/HTMLGetRawText" + \
-        #                '?' + str(params)
-
-        # post_url = "http://access.alchemyapi.com/calls" + "/text/TextGetTextSentiment" + \
-        #            '?' + str(params)
-
-        self.post_url = "http://access.alchemyapi.com/calls/text/TextGetEmotion?outputMode=json&apikey=d39ed152b9ca4e2b4eb9828c0379a44f33ef0f65"
-
-        # content-is-empty error
-        #post_url = "http://access.alchemyapi.com/calls/text/TextGetTargetedSentiment'?outputMode=json&apikey=d39ed152b9ca4e2b4eb9828c0379a44f33ef0f65"
-
-
-    def get_results(self):
+    def get_sentiment(self, text=""):
         """
-        Prints results of call to Watson
+        Returns overall sentiment of provided text
         """
+        post_data = {}
 
-        results = self.s.post(url=self.post_url, data=self.post_data)
+        if not text:
+            return json.dumps({"error": "No text provided"})
 
-        print(results.text)
-        #return results.text
+        post_data['text'] = text
+
+        post_url = "http://access.alchemyapi.com/calls" + "/text/TextGetTextSentiment" + \
+                   '?' + "outputMode=json&apikey=" + self.params['apikey']
+
+        results = self.s.post(url=post_url, data=post_data)
+        return results.text
+
+    def get_emotions(self, text=""):
+        """
+        Returns values for the 5 emotions of provided text
+        """
+        post_data = {}
+
+        if not text:
+            return json.dumps({"error": "No text provided"})
+
+        post_data['text'] = text
+
+        post_url = "http://access.alchemyapi.com/calls" + "/text/TextGetEmotion" + \
+                   '?' + "outputMode=json&apikey=" + self.params['apikey']
+
+        results = self.s.post(url=post_url, data=post_data)
+        return results.text
+
+    def get_top_emotion(self):
+        """
+        Returns top emotion and its value,
+
+        e.g. {"joy": "0.515193"}
+        """
+        res = json.loads(self.get_emotions(text="poopies!"))['docEmotions']
+
+        abc = max(res.iteritems(), key=operator.itemgetter(1))
+
+        return json.dumps({abc[0]: abc[1]})
