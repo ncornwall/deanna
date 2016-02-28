5ac3997165ca5810f5c99da35b393b06c6ca35e5
Natalia <ncornwall@gmail.com>
2016-02-27 21:14:59 -0800

    working on scaling slackbot.

--- test_textemotionanalysis.py
+++ test_textemotionanalysis.py
@@ -5,6 +5,7 @@ Run `python ./test_textemotionanalysis.py` and results should print out
 """
 
 from textemotionanalysis import TextEmotionAnalyzer
+from slackbot import Slackbot
 
 tea = TextEmotionAnalyzer()
 print "get_sentiment:"
@@ -15,3 +16,5 @@ print tea.get_emotions(text="poopies!")
 print "get_top_emotion:"
 print tea.get_top_emotion()
 
+print "slackbot:"
+bot = Slackbot("xoxp-3927713261-3938135231-23401969635-ab0e5635c7")
