f38c9b3011c05bb909a58fa028f20457280b7a29
Natalia <ncornwall@gmail.com>
2016-02-28 09:36:29 -0800

    progress on oauth

--- hello.py
+++ hello.py
@@ -1,5 +1,18 @@
 from flask import Flask
 from flask import render_template
+from flask import request
+from flask_oauth import OAuth
+from flask import session
+
+oauth = OAuth()
+the_remote_app = oauth.remote_app('slack',
+    base_url='https://slack.com/',
+    request_token_url='https://slack.com/api/oauth.access',
+    access_token_url='https://slack.com/api/oauth.access',
+    authorize_url='https://slack.com/api/oauth.access',
+    consumer_key='3927713261.23411567264',
+    consumer_secret='7eff0ae701eb73a07cab2b5c9d091795'
+)
 
 app = Flask(__name__)
 
@@ -7,9 +20,17 @@ app = Flask(__name__)
 def hello_world():
     return render_template('hello.html')
 
-@app.route('/auth')
+@app.route('/auth', methods=['GET', 'POST'])
 def auth():
-    return render_template('hello.html')
+        post_data['text'] = text
+        post_url = "http://slack.oauth.access" 
+        results = self.s.post(url=post_url, data=post_data)
+        return results.text
+
+@slack.tokengetter
+def get_slack_token(token=None):
+	print session.get('slack_token')
+    return session.get('slack_token')
 
 if __name__ == '__main__':
     app.run()
