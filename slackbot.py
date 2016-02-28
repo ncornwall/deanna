64098b62d730cc6dae263a4f37b49213d92607a9
Natalia <ncornwall@gmail.com>
2016-02-27 18:20:34 -0800

    Slackbot now calls watson.

--- slackbot.py
+++ slackbot.py
@@ -1,12 +1,44 @@
 # NWHACKS 2016 AL_C slackbot
 import time
 from slackclient import SlackClient
+import requests
+
+s = requests.Session()
+
+def analyse(textToAnalyze):
+
+	apikey = "d39ed152b9ca4e2b4eb9828c0379a44f33ef0f65"
+	params = {}
+
+	params['apikey'] = apikey
+	params['outputMode'] = 'json'
+
+	post_data = {}
+	post_data['text'] = textToAnalyze
+
+	#post_url = "http://gateway-a.watsonplatform.net/calls/html/HTMLGetRawText" + \
+	#                '?' + str(params)
+
+	# post_url = "http://access.alchemyapi.com/calls" + "/text/TextGetTextSentiment" + \
+	#            '?' + str(params)
+	            
+	post_url = "http://access.alchemyapi.com/calls/text/TextGetEmotion?outputMode=json&apikey=d39ed152b9ca4e2b4eb9828c0379a44f33ef0f65"
+
+	results = s.post(url=post_url, data=post_data)
+
+	print("Result" + results.text)
 
 token = "xoxp-3927713261-3938135231-23401969635-ab0e5635c7"# found at https://api.slack.com/web#authentication
 sc = SlackClient(token)
 if sc.rtm_connect():
     while True:
-        print sc.rtm_read()
+        obj = sc.rtm_read()
+        if len(obj) != 0:
+		    if obj[0].has_key('text'):
+		    	print obj[0]['text']
+		    	analyse(obj[0]['text'])
         time.sleep(1)
 else:
     print "Connection Failed, invalid token?"
+
+# from urllib.parse import urlencode
