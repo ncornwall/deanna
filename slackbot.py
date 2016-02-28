eec4d63a886e97715b284e57874cead900d93daf
tk baylis <tammy.baylis@gmail.com>
2016-02-28 10:53:49 -0800

    Update TEA to ret dict not str. Update API keys, add name=main, fix indents to slackbot.

--- slackbot.py
+++ slackbot.py
@@ -2,39 +2,58 @@
 import time
 from slackclient import SlackClient
 import requests
+from textemotionanalysis import TextEmotionAnalyzer
+
 
 class Slackbot:
 
-	def __init__(self, token):
-		self.s = requests.Session()
+    def __init__(self, token):
+        self.s = requests.Session()
+        self.tea = TextEmotionAnalyzer(outputMode="json")
+        self.token = token
+        sc = SlackClient(token)
+        if sc.rtm_connect():
+            while True:
+                obj = sc.rtm_read()
+                if len(obj) != 0:
+                    if obj[0].has_key('text'):
+                        print obj[0]['text']
+                        print obj[0]['user']
+                        self.analyse(obj[0]['text'], obj[0]['user'])
+                time.sleep(1)
+        else:
+            print "Connection Failed, invalid token?"
 
-		self.token = token
-		sc = SlackClient(token)
-		if sc.rtm_connect():
-		    while True:
-		        obj = sc.rtm_read()
-		        if len(obj) != 0:
-				    if obj[0].has_key('text'):
-				    	print obj[0]['text']
-				    	self.analyse(obj[0]['text'])
-		        time.sleep(1)
-		else:
-		    print "Connection Failed, invalid token?"
+    def analyse(self, textToAnalyze, userId):
 
-	# from urllib.parse import urlencode
-	def analyse(self, textToAnalyze):
+        apikey = "d39ed152b9ca4e2b4eb9828c0379a44f33ef0f65"
+        params = {}
 
-		apikey = "d39ed152b9ca4e2b4eb9828c0379a44f33ef0f65"
-		params = {}
+        params['apikey'] = apikey
+        params['outputMode'] = 'json'
 
-		params['apikey'] = apikey
-		params['outputMode'] = 'json'
+        post_data = {}
+        post_data['text'] = textToAnalyze
 
-		post_data = {}
-		post_data['text'] = textToAnalyze
-		            
-		post_url = "http://access.alchemyapi.com/calls/text/TextGetEmotion?outputMode=json&apikey=d39ed152b9ca4e2b4eb9828c0379a44f33ef0f65"
+        ## Original; prints same as call to get_emotions
+        # post_url = "http://access.alchemyapi.com/calls/text/TextGetEmotion?outputMode=json&apikey=d39ed152b9ca4e2b4eb9828c0379a44f33ef0f65"
+        # results = self.s.post(url=post_url, data=post_data)
+        # print("Result" + results.text)
 
-		results = self.s.post(url=post_url, data=post_data)
+        response = self.tea.get_emotions(textToAnalyze)
+        try:
+            result = response["docEmotions"]
 
-		print("Result" + results.text)
+            # !!! instead of just printing results, spit out / call to user.py or other handler/decisionmaker
+
+            print result
+
+        except:
+            print "Could not get_emotions"
+
+
+
+if __name__ == "__main__":
+    #bot = Slackbot("xoxp-3927713261-3938135231-23401969635-ab0e5635c7")
+    #bot = Slackbot("xoxp-23412134003-23409266740-23415010816-f684006023")
+    bot = Slackbot(token="xoxp-23412134003-23409266740-23444912631-4b1d6ed922")
